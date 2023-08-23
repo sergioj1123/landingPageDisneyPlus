daa9c953e148e3ea244c7fd274abfbbec584e3e6
Sergio Felix Junior <sergiofelix9@gmail.com>
2023-08-23 01:07:27 -0300

    feat: hero section

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 function styles() {
   return gulp
@@ -8,7 +9,18 @@ function styles() {
     .pipe(gulp.dest("./dist/styles"));
 }
 
-exports.default = styles;
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function () {
-  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+  gulp.watch(
+    "./src/styles/*.scss",
+    { ignoreInitial: false },
+    gulp.parallel(styles)
+  );
 };
