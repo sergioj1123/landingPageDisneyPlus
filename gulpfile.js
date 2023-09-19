2a0d40c2a9531d678585141fd5e9f46c68fc17e9
Sergio Felix <sergiofelix9@gmail.com>
2023-09-18 21:30:31 -0300

    feat: responsivite and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+const sourceMaps = require("gulp-sourcemaps");
 
 function styles() {
   return gulp
     .src("./src/styles/*.scss")
+    .pipe(sourceMaps.init())
     .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("./dist/styles"))
+    .pipe(sourceMaps.write(".maps"))
     .pipe(gulp.dest("./dist/styles"));
 }
 
@@ -16,11 +21,18 @@ function images() {
     .pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(styles, images);
+function uglifyjs() {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/scripts"));
+}
+
+exports.default = gulp.parallel(styles, images, uglifyjs);
 exports.watch = function () {
   gulp.watch(
-    "./src/styles/*.scss",
+    ["./src/styles/*.scss", "src/scripts/*.js"],
     { ignoreInitial: false },
-    gulp.parallel(styles)
+    gulp.parallel(styles, uglifyjs)
   );
 };
